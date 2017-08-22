e4d77df7388898937ae395420044261605f7c309
Marcos de Andrade Pinto Filho - 315113653 <315113653@animaedu.intranet>
2017-08-21 22:38:09 -0300

    Adicionando modificações na atividade 06

--- praticasTecWeb/pratica_06/atividade_04/TW-LAB-Pratica06-04.js
+++ praticasTecWeb/pratica_06/atividade_04/TW-LAB-Pratica06-04.js
@@ -2,11 +2,40 @@ function chooseOperation() {
 	var option = document.getElementById("menuOption").value;
 
 	switch(parseInt(option)){
-		case 1: document.getElementById("areaText").innerHTML = "Área de um retângulo"; break;
-		case 2: document.getElementById("areaText").innerHTML = "Área de um triângulo"; break;
-		case 3: document.getElementById("areaText").innerHTML = "Área de um circulo"; break;
-		case 4: document.getElementById("areaText").innerHTML = "Área de um trapézio"; break;
-		default: document.getElementById("areaText").innerHTML = "Você inseriu uma opção incorreta. Tente novamente";
+		case 1: 
+			document.getElementById("areaTitle").innerHTML = "Área de um retângulo";
+			showDiv("firstField");
+			document.getElementById("firstField").innerHTML = "Área de um retângulo";
+			showDiv("secondField");
+			document.getElementById("secondField").innerHTML = "Área de um retângulo";
+			hideDiv("thirdField");
+			break;
+		case 2: 
+			document.getElementById("areaTitle").innerHTML = "Área de um triângulo";
+			showDiv("firstField");
+			document.getElementById("firstField").innerHTML = "Área de um retângulo";
+			showDiv("secondField"); 
+			document.getElementById("secondField").innerHTML = "Área de um retângulo";
+			hideDiv("thirdField");
+			break;
+		case 3: 
+			document.getElementById("areaTitle").innerHTML = "Área de um circulo"; 
+			showDiv("firstField");
+			document.getElementById("firstField").innerHTML = "Área de um retângulo";
+			hideDiv("secondField");
+			hideDiv("thirdField");
+			break;
+		case 4: 
+			document.getElementById("areaTitle").innerHTML = "Área de um trapézio"; 
+			showDiv("firstField");
+			document.getElementById("firstField").innerHTML = "Área de um retângulo";
+			showDiv("secondField");
+			document.getElementById("secondField").innerHTML = "Área de um retângulo";
+			showDiv("thirdField");
+			document.getElementById("thirdField").innerHTML = "Área de um retângulo";
+			break;
+		default: 
+			document.getElementById("areaTitle").innerHTML = "Você inseriu uma opção incorreta. Tente novamente"; 
 	}
 
 	showDiv("calcBox");
@@ -14,4 +43,8 @@ function chooseOperation() {
 
 function showDiv(id) {
    document.getElementById(id).style.display = "block";
+}
+
+function hideDiv(id) {
+   document.getElementById(id).style.display = "none";
 }
