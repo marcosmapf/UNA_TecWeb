27d0d5c0a3ae62dc27551642fd35a3c751440658
Marcos de Andrade Pinto Filho - 315113653 <315113653@animaedu.intranet>
2017-08-28 21:19:02 -0300

    Codigo completo da atividade 06-04 com interface dinâmica

--- praticasTecWeb/pratica_06/atividade_04/TW-LAB-Pratica06-04.js
+++ praticasTecWeb/pratica_06/atividade_04/TW-LAB-Pratica06-04.js
@@ -1,44 +1,94 @@
+var inputFields;
+
 function chooseOperation() {
 	var option = document.getElementById("menuOption").value;
 
 	switch(parseInt(option)){
 		case 1: 
 			document.getElementById("areaTitle").innerHTML = "Área de um retângulo";
-			showDiv("firstField");
+			showInput("firstField");
 			document.getElementById("firstField").innerHTML = "Base do retângulo: " + addNumberInput();
-			showDiv("secondField");
+			showInput("secondField");
 			document.getElementById("secondField").innerHTML = "Altura do retângulo: " + addNumberInput();
-			hideDiv("thirdField");
+			hideInput("thirdField");
+			showDiv("resultBox");
 			break;
 		case 2: 
 			document.getElementById("areaTitle").innerHTML = "Área de um triângulo" ;
-			showDiv("firstField");
+			showInput("firstField");
 			document.getElementById("firstField").innerHTML = "Base do triângulo: " + addNumberInput();
-			showDiv("secondField"); 
+			showInput("secondField"); 
 			document.getElementById("secondField").innerHTML = "Altura do triângulo: " + addNumberInput();
-			hideDiv("thirdField");
+			hideInput("thirdField");
+			showDiv("resultBox");
 			break;
 		case 3: 
 			document.getElementById("areaTitle").innerHTML = "Área de um circulo"; 
-			showDiv("firstField");
+			showInput("firstField");
 			document.getElementById("firstField").innerHTML = "Raio do circulo: " + addNumberInput();
-			hideDiv("secondField");
-			hideDiv("thirdField");
+			hideInput("secondField");
+			hideInput("thirdField");
+			showDiv("resultBox");
 			break;
 		case 4: 
 			document.getElementById("areaTitle").innerHTML = "Área de um trapézio"; 
-			showDiv("firstField");
+			showInput("firstField");
 			document.getElementById("firstField").innerHTML = "Base maior: " + addNumberInput();
-			showDiv("secondField");
+			showInput("secondField");
 			document.getElementById("secondField").innerHTML = "Base menor: " + addNumberInput();
-			showDiv("thirdField");
+			showInput("thirdField");
 			document.getElementById("thirdField").innerHTML = "Altura: " + addNumberInput();
+			showDiv("resultBox");
 			break;
 		default: 
-			document.getElementById("areaTitle").innerHTML = "Você inseriu uma opção incorreta. Tente novamente"; 
+			document.getElementById("areaTitle").innerHTML = "Você inseriu uma opção incorreta. Tente novamente";
+			hideInput("firstField");
+			hideInput("secondField");
+			hideInput("thirdField");
+			hideDiv("resultBox");
 	}
-
 	showDiv("calcBox");
+}
+
+function calculateArea(){
+	menuOption = document.getElementById("areaTitle").innerHTML;
+	switch (menuOption){
+		case "Área de um retângulo": 
+			var n1 = document.getElementById("inputBox1").value;
+			var n2 = document.getElementById("inputBox2").value;
+			document.getElementById("areaResult").innerHTML = n1*n2;
+			break;
+		case "Área de um triângulo": 
+			var n1 = document.getElementById("inputBox1").value;
+			var n2 = document.getElementById("inputBox2").value;
+			document.getElementById("areaResult").innerHTML = ((n1*n2)/2);
+			break;
+		case "Área de um circulo": 
+			var n1 = document.getElementById("inputBox1").value;
+			document.getElementById("areaResult").innerHTML = (Math.pow(n1,2) * Math.PI);
+			break;
+		case "Área de um trapézio": 
+			var n1 = parseInt(document.	getElementById("inputBox1").value);
+			var n2 = parseInt(document.getElementById("inputBox2").value);			
+			var n3 = document.getElementById("inputBox3").value;
+			document.getElementById("areaResult").innerHTML = (n1+n2)*n3/2;
+			break;
+		default:
+	}
+}
+
+function showInput(id) {
+   document.getElementById(id).style.display = "block";
+
+   switch (id){
+   	case "firstField": inputFields = 1; break;
+   	case "secondField": inputFields = 2; break;
+   	case "thirdField": inputFields = 3; break;
+   }
+}
+
+function hideInput(id) {
+   document.getElementById(id).style.display = "none";
 }
 
 function showDiv(id) {
@@ -50,5 +100,5 @@ function hideDiv(id) {
 }
 
 function addNumberInput(){
-	return "<input type=\"number\">"
+	return "<input id=\"inputBox" + inputFields + "\" type=\"number\">"
 }
