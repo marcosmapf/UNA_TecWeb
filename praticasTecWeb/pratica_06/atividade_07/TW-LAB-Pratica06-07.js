af6b75146415d3b7fe254a40f03217fead6c6e3d
Marcos de Andrade <marcosscamor@hotmail.com>
2017-09-11 03:03:15 -0300

    adicionando atividade 07 e 08 da prática 06

--- praticasTecWeb/pratica_06/atividade_07/TW-LAB-Pratica06-07.js
+++ praticasTecWeb/pratica_06/atividade_07/TW-LAB-Pratica06-07.js
@@ -6,10 +6,10 @@ function sumMatrix(){
 		valores[i] = parseInt(document.getElementById("inputField" + i).value);
 	}
 
-	document.getElementById("resultField0").innerHTML = valores[0] + valores[4];
-	document.getElementById("resultField1").innerHTML = valores[1] + valores[5];
-	document.getElementById("resultField2").innerHTML = valores[2] + valores[6];
-	document.getElementById("resultField3").innerHTML = valores[3] + valores[7];
+	document.getElementById("resultField0").value = valores[0] + valores[4];
+	document.getElementById("resultField1").value = valores[1] + valores[5];
+	document.getElementById("resultField2").value = valores[2] + valores[6];
+	document.getElementById("resultField3").value = valores[3] + valores[7];
 
 	showDiv("results");
 }
