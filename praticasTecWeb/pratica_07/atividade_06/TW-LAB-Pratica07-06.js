1b3681dafb126142535ddf00232d1ed3cacac77b
Marcos de Andrade Pinto Filho - 315113653 <315113653@animaedu.intranet>
2017-10-02 21:17:01 -0300

    Adicionando lista 07 completa

--- praticasTecWeb/pratica_07/atividade_06/TW-LAB-Pratica07-06.js
+++ praticasTecWeb/pratica_07/atividade_06/TW-LAB-Pratica07-06.js
@@ -1,9 +1,10 @@
+var supplierArray = new Array();
+var contador = 0;
+
 window.onload = function () {
     'Use strict'
-
     clearScreen();
-    var i = requestSupplierAmmount();     
-    alert(i);
+    requestSupplierAmmount();     
 }
 
 function requestSupplierAmmount(){
@@ -11,11 +12,33 @@ function requestSupplierAmmount(){
     var j = parseInt(window.prompt("Insira o número de fornecedores a serem adicionados"));
     if (isNaN(j)){
         window.alert("Insira um valor válido");
-        //setTimeout(requestSupplierAmmount, 1000);
+        requestSupplierAmmount();
+    }
+    else if (j == 0){
+        printSuppliers(contador);
     }
     else{
-        return j;
+        for (var i = 0; i < j; i ++){
+            var supplier = new Object();
+            supplier.nome = window.prompt("Nome");
+            supplier.telefone = window.prompt("Telefone");
+            supplier.cnpj = window.prompt("CNPJ");
+            supplierArray[contador] = supplier;
+            contador++;
+        }
+        requestSupplierAmmount();
     }
 }
 
 function clearScreen(){document.body.innerHTML = "";}
+
+function printSuppliers(j){
+    for (var i = 0; i < j; i++){
+        document.write (    
+            "Fornecedor: " + supplierArray[i].nome + "<br/>" + 
+            "Telefone: " + supplierArray[i].telefone + "<br/>" + 
+            "CNPJ: " + supplierArray[i].cnpj + "<br/>" +
+            "----------------------<br/>" 
+        );
+    }
+}
